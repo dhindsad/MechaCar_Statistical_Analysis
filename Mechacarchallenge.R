427d62d6087c7bd86f0c6c5b8f359781bff67064
Deepanshu Dhindsa <67085043+dhindsad@users.noreply.github.com>
2020-11-05 21:56:24 -0500

    Add files via upload

---
+++ Mechacarchallenge.R
@@ -0,0 +1,9 @@
+
+installed.packages()
+library(dplyr)
+demo_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance+ AWD,data=MechaCar_mpg) #generate multiple linear regression model
+summary(lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance+ AWD,data=MechaCar_mpg))
+
+
+
